6a4f700f6b76067d1217f2adc512626b0fdf4eb7
Olaf Nekeman <olafnekeman@svinergy.com>
2022-07-11 22:54:34 +0200

    Added a simulation to dashboard.

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
+"""
+This file holds the interface for the data streaming interface.
+"""
+import numpy as np
+import datetime as dt
 from dash import Dash, dcc, html, Input, Output
-import os
+import plotly.graph_objs as go
+from src.classes.heat_pump import HeatPump
 
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
@@ -8,19 +14,97 @@ app = Dash(__name__, external_stylesheets=external_stylesheets)
 
 server = app.server
 
+""" -------------------
+Object creation
+------------------------ """
+
+heat_pump = HeatPump(
+    t_set=19,
+    t_range=0.5,
+    p_compressor=2,
+    demo_mode=True
+)
+
+# Demo data
+x_start = heat_pump.connection.state.timestamp
+y_start = heat_pump.connection.state.t_inside
+figure = dict(
+    data=[{'x': [x_start], 'y': [y_start]}],
+    layout=dict(xaxis=dict(range=[x_start, x_start + dt.timedelta(minutes=5)]),
+                yaxis=dict(range=[15, 23])))
+
+
 app.layout = html.Div([
     html.H2('Hello World'),
     dcc.Dropdown(['LA', 'NYC', 'MTL'],
-        'LA',
-        id='dropdown'
+                 'LA',
+                 id='dropdown'
+                 ),
+    html.Div(id='display-value'),
+    html.Div(
+        dcc.Input(
+                id="input_tado_email",
+                type='email',
+                placeholder="input type email",
+        )
     ),
-    html.Div(id='display-value')
+    html.Div(
+        dcc.Input(
+            id="input_tado_password",
+            type='password',
+            placeholder="input type password",
+        )
+    ),
+    html.Button('Submit', id='submit-val', n_clicks=0),
+    html.Div(id='container-button-basic',
+             children='Enter a value and press submit'),
+
+    dcc.Graph(id='graph', figure=figure),
+    dcc.Interval(id="interval")
 ])
 
+
+
+
+
+@app.callback(Output('graph', 'extendData'), [Input('interval', 'n_intervals')])
+def update_data(n_intervals):
+    heat_pump.get_current_state()
+
+    x_data = heat_pump.connection.state.timestamp
+    y_point = heat_pump.connection.state.t_inside
+
+    print(x_data, y_point)
+
+    # tuple is (dict of new data, target trace index, number of points to keep)
+    return dict(x=[[x_data]], y=[[y_point]]), [0], 100
+
+
+""" -------------------------
+Callbacks
+------------------------- """
+
+@app.callback(
+    Output('container-button-basic', 'children'),
+    Input('submit-val', 'n_clicks')
+)
+def update_output(n_clicks):
+    heat_pump.get_current_state()
+    return 'The input value was unkonwn and the button has been clicked {} times'.format(
+
+        round(heat_pump.connection.state.t_inside, 2)
+    )
+
+@server.route('/greet')
+def say_hello():
+    return 'Hello from Server'
+
+
 @app.callback(Output('display-value', 'children'),
-                [Input('dropdown', 'value')])
+              [Input('dropdown', 'value')])
 def display_value(value):
     return f'You have selected {value}'
+
 
 if __name__ == '__main__':
     app.run_server(debug=True)
---
+++ src/classes/heat_pump.py
@@ -0,0 +1,135 @@
+"""
+This file holds the heat pump simulation class.
+"""
+import datetime as dt
+
+
+class State:
+    def __init__(self, t_inside: float, t_inside_exp: float, t_outside: float):
+        # Temperatures
+        self.t_inside = t_inside
+        self.t_inside_exp = t_inside_exp
+        self.t_outside = t_outside
+
+        # Other
+        self.timestamp = dt.datetime.utcnow()
+
+    @property
+    def t_delta(self):
+        """
+        Temperature difference between indoor and outdoor.
+        """
+        return self.t_inside - self.t_outside
+
+
+class HeatPumpConnect:
+    def __init__(self, t_set, t_range, p_compressor, initial_state: State):
+        # Thermostat settings
+        self.t_set = t_set
+        self.t_range = t_range
+
+        # Heat pump variables
+        self.p_compressor = p_compressor
+        self.cop = 3.5
+
+        self.state = initial_state
+
+    def get_state(self):
+        """
+        Asks the state of the heat pump system.
+        """
+        pass
+
+
+class VaillantConnect(HeatPumpConnect):
+    def __init__(self, t_set, t_range, p_compressor, initial_state):
+        super(VaillantConnect, self).__init__(t_set, t_range, p_compressor, initial_state)
+
+    def get_state(self):
+        """
+        Asks the state of a Vaillant heat pump system.
+        """
+        pass
+
+
+class DemoConnect(HeatPumpConnect):
+    def __init__(self, t_set, t_range, p_compressor, initial_state, k=1e-3):
+        self.k = k  # in [
+        super(DemoConnect, self).__init__(t_set, t_range, p_compressor, initial_state)
+
+    def get_state(self):
+        """
+        Creates a demo state.
+        """
+        passed_time = dt.datetime.utcnow() - self.state.timestamp
+        t_inside_next = self.get_next_t_inside(
+            t_inside=self.state.t_inside,
+            t_delta=self.state.t_delta,
+            time=passed_time.seconds / 60
+        )
+
+        self.state = State(
+            t_inside=t_inside_next,
+            t_inside_exp=t_inside_next,
+            t_outside=self.state.t_outside
+        )
+
+    def get_next_t_inside(self, t_inside, t_delta, time):
+        """
+        Calculates the next inside temperature.
+        """
+        return t_inside - t_delta * self.k * time
+
+
+class HeatPump:
+
+    def __init__(self, t_set, t_range, p_compressor, demo_mode=False):
+
+        if demo_mode:
+            self.connection = DemoConnect(
+                t_set=t_set,
+                t_range=t_range,
+                p_compressor=p_compressor,
+                initial_state=State(
+                    t_inside=19.2,
+                    t_inside_exp=19.2,
+                    t_outside=14
+                ),
+                k=1e-1
+            )
+        else:
+            self.connection = VaillantConnect()
+
+    def get_current_state(self):
+        """
+        Retrieves the current state from the connected source.
+        """
+        self.connection.get_state()
+        pass
+
+    def get_forecast(self):
+        """
+        Get the forecast.
+        """
+        pass
+
+    def update_forecast(self):
+        """
+        Updates the heat pump energy demand forecast.
+        """
+        pass
+
+
+if __name__ == "__main__":
+    heat_pump = HeatPump(
+        t_set=19,
+        t_range=0.5,
+        p_compressor=2,
+        demo_mode=True
+    )
+
+    print("t_inside: ", heat_pump.connection.state.t_inside)
+    heat_pump.connection.state.timestamp += dt.timedelta(hours=-1)
+    heat_pump.connection.get_state()
+    print("t_inside: ", heat_pump.connection.state.t_inside)
+
