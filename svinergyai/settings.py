e9a74e743affdf7475a9702d4f9931ed70ab5b7b
Olaf Nekeman <olafnekeman@svinergy.com>
2022-07-20 22:20:03 +0200

    v5

--- svinergyai/settings.py
+++ svinergyai/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-=o3#%8xoip!e=v9$qlhdbhu1nwmb#6cnp5^8q-xrk#o5&xkn4@
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'https://svinergy-ai-app-test.herokuapp.com/']
+ALLOWED_HOSTS = ['127.0.0.1', 'svinergy-ai-app-test.herokuapp.com']
 
 
 # Application definition
