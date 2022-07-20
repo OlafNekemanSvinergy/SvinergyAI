7c16e912ea36799b696f665ced55538be1075827
Olaf Nekeman <olafnekeman@svinergy.com>
2022-07-20 22:16:31 +0200

    v4

--- svinergyai/settings.py
+++ svinergyai/settings.py
@@ -120,7 +120,7 @@ USE_TZ = True
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
-print(STATIC_ROOT)
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
