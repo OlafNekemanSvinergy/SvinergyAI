46a3a3425421e9f8940992059c78bdfa59962ff3
olafnekeman <olafnekeman@svinergy.com>
2022-07-24 14:12:59 +0200

    Updated ALLOWED_HOSTS and SECRET_KEY

--- svinergyai/settings.py
+++ svinergyai/settings.py
@@ -21,12 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-=o3#%8xoip!e=v9$qlhdbhu1nwmb#6cnp5^8q-xrk#o5&xkn4@'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
+print(SECRET_KEY)
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['127.0.0.1', 'svinergy-ai-app-test.herokuapp.com']
+ALLOWED_HOSTS = ['0.0.0.0','127.0.0.1', 'svinergy-ai-app-test.herokuapp.com']
 
 
 # Application definition
