0368a78a4667d94085570c870a7d87298703282b
olafnekeman <olafnekeman@svinergy.com>
2022-07-30 19:45:29 +0200

    fix 3

--- svinergyai/urls.py
+++ svinergyai/urls.py
@@ -31,5 +31,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
