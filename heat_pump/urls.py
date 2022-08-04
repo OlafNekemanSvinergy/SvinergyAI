ac31f0fe18b02a56a3e61fc9ae4e06c169eceec7
olafnekeman <olafnekeman@svinergy.com>
2022-08-04 18:59:01 +0200

    updated the serializers

--- heat_pump/urls.py
+++ heat_pump/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
 from heat_pump import views
 
 urlpatterns = [
     path('heatPumps/', views.connection_list),
     path('heatPumps/<int:pk>/', views.connection_detail),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- heat_pump/views.py
+++ heat_pump/views.py
@@ -1,50 +1,50 @@
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 from heat_pump.models import Connection
 from heat_pump.serializers import ConnectionSerializer
 
 # Create your views here.
-@csrf_exempt
-def connection_list(request):
+@api_view(['GET', 'POST'])
+def connection_list(request, format=None):
     """
-    List all connections, or create a new connection.
+    List all code connections, or create a new connection.
     """
     if request.method == 'GET':
         connections = Connection.objects.all()
         serializer = ConnectionSerializer(connections, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = ConnectionSerializer(data=data)
+        serializer = ConnectionSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
-
-@csrf_exempt
-def connection_detail(request, pk):
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+@api_view(['GET', 'PUT', 'DELETE'])
+def connection_detail(request, pk, format=None):
     """
-    Retrieve, update or delete a code connection.
+    Retrieve, update or delete a code connections.
     """
     try:
-        connection = Connection.objects.get(pk=pk)
+        connections = Connection.objects.get(pk=pk)
     except Connection.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
     if request.method == 'GET':
-        serializer = ConnectionSerializer(connection)
-        return JsonResponse(serializer.data)
+        serializer = ConnectionSerializer(connections)
+        return Response(serializer.data)
 
     elif request.method == 'PUT':
-        data = JSONParser().parse(request)
-        serializer = ConnectionSerializer(connection, data=data)
+        serializer = ConnectionSerializer(connections, data=request.data)
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data)
-        return JsonResponse(serializer.errors, status=400)
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     elif request.method == 'DELETE':
-        connection.delete()
-        return HttpResponse(status=204)
+        connections.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
